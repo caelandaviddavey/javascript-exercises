054086d730599e152284a6be519bd8edbfaac429
caelandaviddavey <191670161+caelandaviddavey@users.noreply.github.com>
2025-01-11 13:08:19 -0500

    exercises (vague comment :3)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
 const repeatString = function() {
+    let string = 'bye';
+    let num = 0;
+    let result = '';
 
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
