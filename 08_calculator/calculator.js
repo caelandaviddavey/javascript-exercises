ac4eb79fd2c270fac3408da6c7983f6349677b0b
caelandaviddavey <191670161+caelandaviddavey@users.noreply.github.com>
2025-01-26 01:37:29 -0500

    Answered TOP questions.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let addedNumbers = a + b;
+  return addedNumbers;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let subtractedNumber = a - b;
+  return subtractedNumber;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num, 1);
+};
+
+const power = function(a, b) {
+	return a ** b;
+};
+
+const factorial = function(n) {
+	let result = 1;
+  if (n === 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+
+
+
+
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) return "OOPS";
+    if (n <= 1) return n;
+    let a = 0, b = 1, next;
+    for (let i = 2; i <= n; i++) {
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 
 };
 
